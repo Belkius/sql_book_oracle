f483df06c37d36acf47082f67f66686a6dee3ff4
Grzegorz Belkius <118485347+Belkius@users.noreply.github.com>
2023-10-17 21:22:59 +0200

    Oracle create_retail_sales_table.sql done

--- Chapter 3: Time Series Analysis/create_retail_sales_table.sql
+++ Chapter 3: Time Series Analysis/create_retail_sales_table.sql
@@ -1,18 +1,25 @@
--- create the table
-DROP table if exists retail_sales;
-CREATE table retail_sales
-(
-sales_month date
-,naics_code varchar
-,kind_of_business varchar
-,reason_for_null varchar
-,sales decimal
-)
-;
+-- delete the table if it already exists
 
--- populate the table with data from the csv file. Download the file locally before completing this step
-COPY retail_sales 
-FROM '/localpath/us_retail_sales.csv' -- change to the location you saved the csv file
-DELIMITER ','
-CSV HEADER
-;
+BEGIN
+	EXECUTE IMMEDIATE 'DROP TABLE retail_sales';
+EXCEPTION
+	WHEN OTHERS THEN
+		IF SQLCODE != -942 THEN
+			RAISE;
+		END IF;
+END;
+
+-- create the table
+
+CREATE TABLE retail_sales(
+sales_month date,
+naics_code varchar(50),
+kind_of_business varchar(100),
+reason_for_null varchar(50),
+sales decimal
+);
+
+-- Import the data in one of three methods Oracle gives you:
+-- 1. Use the SQLDeveloper wizard (right-click table > import)
+-- 2. Use SQL*Loader (must have SQL*Plus installed)
+-- 3. Define the CSV file as an external table
