9535e2a37b6e8a7586d5540a6b995763846273f6
Grzegorz Belkius <118485347+Belkius@users.noreply.github.com>
2023-10-19 22:42:08 +0200

    Oracle create_legislators_table.sql done

--- Chapter 4: Cohorts/create_legislators_table.sql
+++ Chapter 4: Cohorts/create_legislators_table.sql
@@ -1,42 +1,51 @@
-DROP table if exists legislators;
-
-CREATE table legislators
+-- delete the table if it already exist
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE legislators';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+ 
+-- create the table
+CREATE TABLE legislators
 (
-full_name varchar--name_official_full
-,first_name varchar --name_first
-,last_name varchar --name_last
-,middle_name varchar --name_middle
-,nickname varchar --name_nickname
-,suffix varchar --name_suffix
-,other_names_end date -- other_names_0_end date
-,other_names_middle varchar -- other_names_0_middle
-,other_names_last varchar -- other_names_0_last
-,birthday date -- bio_birthday
-,gender varchar-- bio_gender
-,id_bioguide varchar primary key
-,id_bioguide_previous_0 varchar
-,id_govtrack int
-,id_icpsr int
-,id_wikipedia varchar
-,id_wikidata varchar
-,id_google_entity_id varchar
-,id_house_history bigint
-,id_house_history_alternate int
-,id_thomas int
-,id_cspan int
-,id_votesmart int
-,id_lis varchar
-,id_ballotpedia varchar
-,id_opensecrets varchar
-,id_fec_0 varchar
-,id_fec_1 varchar
-,id_fec_2 varchar
-)
-;
-
-COPY legislators
-FROM '/localpath/legislators.csv' -- change to the location you saved the csv file
-DELIMITER ','
-CSV HEADER
-;
-
+full_name VARCHAR(50),
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+middle_name VARCHAR(50),
+nickname VARCHAR(50),
+suffix VARCHAR(50),
+other_names_end DATE,
+other_names_middle VARCHAR(50),
+other_names_last VARCHAR(50),
+birthday DATE,
+gender VARCHAR(50),
+id_bioguide VARCHAR(50) NOT NULL,
+id_bioguide_previous_0 VARCHAR(50),
+id_govtrack int,
+id_icpsr int,
+id_wikipedia VARCHAR(60),
+id_wikidata VARCHAR(50),
+id_google_entity_id VARCHAR(50),
+id_house_history int,
+id_house_history_alternate int,
+id_thomas int,
+id_cspan int,
+id_votesmart int,
+id_lis VARCHAR(50),
+id_ballotpedia VARCHAR(50),
+id_opensecrets VARCHAR(50),
+id_fec_0 VARCHAR(50),
+id_fec_1 VARCHAR(50),
+id_fec_2 VARCHAR(50),
+CONSTRAINT bioguide_pk PRIMARY KEY (id_bioguide)
+);
+ 
+ 
+-- import the data in one of three methods Oracle gives you:
+-- 1. Use the SQLDeveloper wizard (right-click table > import)
+-- 2. Use SQL*Loader (must have SQL*Plus installed)
+-- 3. Define the CSV file as an external table
+ 
