179ab32f0dccbadaa68dc74be10db897d6c16599
Grzegorz Belkius <118485347+Belkius@users.noreply.github.com>
2023-10-19 22:42:58 +0200

    Oracle create_legislators_terms.sql done

--- Chapter 4: Cohorts/create_legislators_terms.sql
+++ Chapter 4: Cohorts/create_legislators_terms.sql
@@ -1,33 +1,42 @@
-DROP table if exists legislators_terms;
-CREATE table legislators_terms
+-- delete the table if it already exist
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE legislators_terms';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+ 
+-- create the table
+CREATE TABLE legislators_terms
 (
-id_bioguide varchar
-,term_number int 
-,term_id varchar primary key
-,term_type varchar
-,term_start date
-,term_end date
-,state varchar
-,district int
-,class int
-,party varchar
-,how varchar
-,url varchar--terms_1_url
-,address varchar --terms_1_address
-,phone varchar --terms_1_phone
-,fax varchar --terms_1_fax
-,contact_form varchar --terms_1_contact_form
-,office varchar--terms_1_office
-,state_rank varchar --terms_1_state_rank
-,rss_url varchar --terms_1_rss_url
-,caucus varchar -- terms_1_caucus
-)
-;
-
-COPY legislators_terms
-FROM '/localpath/legislators_terms.csv' -- change to the location you saved the csv file
-DELIMITER ','
-CSV HEADER
-;
-
-
+id_bioguide VARCHAR(7),
+term_number int,
+term_id VARCHAR(12) NOT NULL,
+term_type VARCHAR(3),
+term_start DATE,
+term_end DATE,
+state VARCHAR(2),
+district int,
+class int,
+party VARCHAR(35),
+how VARCHAR(25),
+url VARCHAR(50),
+address VARCHAR(70),
+phone VARCHAR(15),
+fax VARCHAR(15),
+contact_form VARCHAR(150),
+office VARCHAR(50),
+state_rank VARCHAR(25),
+rss_url VARCHAR(150),
+caucus VARCHAR(25),
+CONSTRAINT term_pk PRIMARY KEY (term_id)
+);
+ 
+ 
+-- import the data in one of three methods Oracle gives you:
+-- 1. Use the SQLDeveloper wizard (right-click table > import)
+-- 2. Use SQL*Loader (must have SQL*Plus installed)
+-- 3. Define the CSV file as an external table
+ 
