41b1f565a309979e2d3e26d050a4289e5598f677
Grzegorz Belkius <118485347+Belkius@users.noreply.github.com>
2023-10-30 16:23:59 +0100

    Oracle create_videogame_sales.sql done

--- Chapter 8: Creating Complex Data Sets/create_videogame_sales.sql
+++ Chapter 8: Creating Complex Data Sets/create_videogame_sales.sql
@@ -1,19 +1,31 @@
-DROP table if exists videogame_sales;
+-- delete the table if it already exist
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE videogame_sales';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+ 
+-- create the table
 CREATE table videogame_sales
 (
-rank int
-,name varchar
-,platform varchar
-,year int
-,genre varchar
-,publisher varchar
-,na_sales decimal
-,eu_sales decimal
-,jp_sales decimal
-,other_sales decimal
-,global_sales decimal
-)
-;
-
--- change localpath to the location of the saved file
-COPY videogame_sales FROM '/localpath/videogame_sales.csv' DELIMITER ',' CSV HEADER;
+rank int,
+name varchar(200),
+platform varchar(20),
+year int,
+genre varchar(40),
+publisher varchar(40),
+na_sales decimal(10, 2),
+eu_sales decimal(10, 2),
+jp_sales decimal(10, 2),
+other_sales decimal(10, 2),
+global_sales decimal(10, 2)
+);
+ 
+-- import the data in one of three methods Oracle gives you:
+-- 1. Use the SQLDeveloper wizard (right-click table > import)
+-- 2. Use SQL*Loader (must have SQL*Plus installed)
+-- 3. Define the CSV file as an external table
+ 
