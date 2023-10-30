360b2d351ce79e31a16bc7fab6d70d3574f441ae
Grzegorz Belkius <118485347+Belkius@users.noreply.github.com>
2023-10-30 16:14:41 +0100

    Oracle create_earthquakes_table.sql done

--- Chapter 6: Anomaly Detection/create_earthquakes_table.sql
+++ Chapter 6: Anomaly Detection/create_earthquakes_table.sql
@@ -1,59 +1,41 @@
-DROP table if exists public.earthquakes;
-CREATE table public.earthquakes
+-- delete the table if it already exist
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE earthquakes';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+ 
+-- create the table
+CREATE table earthquakes
 (
-time timestamp
-,latitude decimal
-,longitude decimal
-,depth decimal
-,mag decimal
-,magType varchar
-,nst decimal
-,gap decimal
-,dmin decimal
-,rms decimal
-,net varchar
-,id varchar
-,updated timestamp
-,place varchar
-,type varchar
-,horizontalError decimal
-,depthError decimal
-,magError decimal
-,magNst decimal
-,status varchar
-,locationSource varchar
-,magSource varchar
-)
-;
-
--- replace localpath with the directory where you saved the csv files. The data set is large - the code will run if you load one or a few files, but the results will differ.
-
-COPY public.earthquakes FROM '/localpath/earthquakes1.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes2.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes3.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes4.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes5.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes6.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes7.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes8.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes9.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes10.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes11.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes12.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes13.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes14.csv' DELIMITER ',' CSV HEADER;
-
-COPY public.earthquakes FROM '/localpath/earthquakes15.csv' DELIMITER ',' CSV HEADER;
+time timestamp,
+latitude number(20,10),
+longitude number(20,10),
+depth number(20,10),
+mag number(20,10),
+magType varchar2(50),
+nst number(20,10),
+gap number(20,10),
+dmin number(20,10),
+rms number(20,10),
+net varchar2(50),
+id varchar2(50),
+updated timestamp,
+place varchar2(200),
+type varchar2(50),
+horizontalError number(20,10),
+depthError number(20,10),
+magError number(20,10),
+magNst number(20,10),
+status varchar2(50),
+locationSource varchar2(50),
+magSource varchar2(50)
+);
+ 
+-- import the data in one of three methods Oracle gives you:
+-- 1. Use the SQLDeveloper wizard (right-click table > import)
+-- 2. Use SQL*Loader (must have SQL*Plus installed)
+-- 3. Define the CSV file as an external table
